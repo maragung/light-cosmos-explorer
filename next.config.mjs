ff0ac16eb96f0c52ad9cb189030c337fa3f15911
Maragung <50156713+maragung@users.noreply.github.com>
2025-10-17 11:55:23 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/',
+      },
+    ];
+  },
+}
+
 
 export default nextConfig;
