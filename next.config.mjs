0ae3bb5028cb7e4eb465552ca54173afa1e79d11
Maragung <50156713+maragung@users.noreply.github.com>
2025-10-17 11:58:24 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // Hapus swcMinify
   async rewrites() {
     return [
       {
@@ -13,5 +12,4 @@ const nextConfig = {
   },
 }
 
-
-export default nextConfig;
+export default nextConfig
