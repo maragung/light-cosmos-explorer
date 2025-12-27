282291a32090ef1132f374436ce9d462d802e885
Maragung <50156713+maragung@users.noreply.github.com>
2025-12-27 08:45:59 +0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  /***async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: '/',
-      },
-    ];
-  },***/
-}
+    reactStrictMode: true,
+};
 
-export default nextConfig
+export default nextConfig;
